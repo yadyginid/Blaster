9e788a50c43e8ec57920f2d771a12c5fc7db0abb
yadyginid <yadyginid@gmail.com>
2023-02-19 19:56:04 +0300

    add move for character

---
+++ Source/Blaster/Private/Character/BaseCharacter.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/BaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+    CameraBoom->SetupAttachment(GetMesh());
+    CameraBoom->TargetArmLength = 600.0f;
+    CameraBoom->bUsePawnControlRotation = true;
+    
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>("FollowCamera");
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
+}
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+    
+    PlayerInputComponent->BindAxis("MoveForward", this, &ABaseCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &ABaseCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("LookUp", this, &ABaseCharacter::LookUp);
+    PlayerInputComponent->BindAxis("Turn", this, &ABaseCharacter::Turn);
+}
+
+void ABaseCharacter::MoveForward(float Amount)
+{
+    if (!Controller || Amount == 0.0f)
+    {
+        return;
+    }
+
+    const FRotator YawRotation(0.0f, Controller->GetControlRotation().Yaw, 0.f);
+    const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+
+    AddMovementInput(Direction, Amount);
+}
+
+void ABaseCharacter::MoveRight(float Amount)
+{
+    if (!Controller || Amount == 0.0f)
+    {
+        return;
+    }
+
+    const FRotator YawRotation(0.0f, Controller->GetControlRotation().Yaw, 0.f);
+    const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+
+    AddMovementInput(Direction, Amount);
+}
+
+void ABaseCharacter::Turn(float Amount)
+{
+    if (!Controller || Amount == 0.0f)
+    {
+        return;
+    }
+
+    AddControllerYawInput(Amount);
+}
+
+void ABaseCharacter::LookUp(float Amount)
+{
+    if (!Controller || Amount == 0.0f)
+    {
+        return;
+    }
+
+    AddControllerPitchInput(Amount);
+}
---
+++ Source/Blaster/Public/Character/BaseCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class BLASTER_API ABaseCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    ABaseCharacter();
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+    virtual void BeginPlay() override;
+    
+    void MoveForward(float Amount);
+    void MoveRight(float Amount);
+    void Turn(float Amount);
+    void LookUp(float Amount);
+
+private:
+    UPROPERTY(VisibleAnywhere, Category = Camera)
+    USpringArmComponent* CameraBoom;
+
+    UPROPERTY(VisibleAnywhere, Category = Camera)
+    UCameraComponent* FollowCamera;
+
+};
